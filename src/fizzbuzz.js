7a6f160a31c5d54c94d15b5348c2848551e5b2dc
axeljunior1 <68278948+axeljunior1@users.noreply.github.com>
2022-01-14 16:51:40 +0100

    Test + modif FizzBuzz

--- src/fizzbuzz.js
+++ src/fizzbuzz.js
@@ -1,15 +1,23 @@
 function fizzBuzz (n) {
     // TODO: Compléter fizzbuzz()
     
-    if (n%3==0) return "Fizz";
-
-    if (n%5==0) return "Buzz";
-
-    if (n%3==0 && n%5==0) {
-        return "FizzBuzz"
-    } else {
-        return null
+    if (n%3==0){
+        if (n%5==0) {
+            return "FizzBuzz";
+        }else{
+            return "Fizz";
+        }
     }
+    if (n%5==0){
+        if (n%3==0) {
+            return "FizzBuzz";
+        }else{
+            return "Buzz";
+        }
+    }
+    
+    return 'null'
+    
 }
 
 module.exports = fizzBuzz
--- test/fizzbuzz.test.js
+++ test/fizzbuzz.test.js
@@ -1,6 +1,18 @@
 const fizzBuzz = require("../src/fizzbuzz")
 
-test('', () => {
+test('FB response is null', () => {
     // TODO: Tester fizzbuzz()
-
+    expect(fizzBuzz(11).toBe('null'));
+})
+test('FB response is Fizz', () => {
+    // TODO: Tester fizzbuzz()
+    expect(fizzBuzz(3).toBe('Fizz'));
+})
+test('FB response is Buzz', () => {
+    // TODO: Tester fizzbuzz()
+    expect(fizzBuzz(5).toBe('Buzz'));
+})
+test('FB response is FizzBuzz', () => {
+    // TODO: Tester fizzbuzz()
+    expect(fizzBuzz(15).toBe('FizzBuzz'));
 })
