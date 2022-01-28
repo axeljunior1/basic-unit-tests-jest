b6755e3b0f8d56daadc1033d6f508ed78592b861
axeljunior1 <68278948+axeljunior1@users.noreply.github.com>
2022-01-28 19:30:10 +0100

    Correction du code MergeArrays et test unitaires réussi

--- src/mergeSortedArrays.js
+++ src/mergeSortedArrays.js
@@ -1,48 +1,44 @@
 function mergeSortedArrays(tab1, tab2){
-    // variable pour la longueur des tableau 
-    let len1 = tab1.length; 
-    let len2 = tab2.length;
-    // var et boucle pour le 
-    //exchange table 
 
-    let t = [];
-    if (len1<len2) { // permuter pour avoir le tab 1 comme le tab le plus long
-        t = tab1;
-        tab1 = tab2;
-        tab2 = t ; 
-    }
-    let tab = []; // tableau vide 
-    
-    len1 = tab1.length; 
+    len1 = tab1.length;
     len2 = tab2.length;
-    // initialisation de deux compteurs 
+    let mytab = [];
+    let t = [];
+
+    if (len1 > len2 ) {
+        t = tab1;
+        tab1 =tab2;
+        tab2 = t ;
+
+        len1 = tab1.length;
+        len2 = tab2.length;
+    }
     let i = 0;
     let j = 0;
+
+
     do {
-        if (tab1[i]< tab2[j]) {
-            tab.push(tab1[i]);
-            i = i + 1;
+        if(tab1[i]<tab2[j]){
+            mytab.push(tab1[i])
+            i++;
         }
-        if (tab1[i]> tab2[j]) {
-            tab.push(tab2[j]);
-            j = j + 1;
-        } 
-        if (tab1[i] === tab2[j]) {
-            tab.push(tab1[i]);
-            tab.push(tab2[j]);
-            i = i + 1;
-            j = j + 1;
-        }
-    } while (i<len2 && j < len2);
-
-        for (let index = i; index < len1; index++) {
-
-            tab.push(tab1[i]);
-            i = i + 1;
+        if(tab1[i]>tab2[j]){
+            mytab.push(tab2[j])
+            j++;
         }
 
-    return tab ;
+    } while (i<len1 && j < len2);
+    if(i < len1){
+        for (let k = i; k < len1 ; k++) {
+            mytab.push(tab1[k])
+        }
+    }else {
+        for (let k = j; k < len2; k++) {
+            mytab.push(tab2[k])
+        }
+    }
 
+    return mytab ;
 }
 
 module.exports = mergeSortedArrays ; 
