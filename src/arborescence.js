6a6cb42609a5bdaf9ebf431285fdcd15e6f239b5
axeljunior1 <68278948+axeljunior1@users.noreply.github.com>
2022-01-29 19:51:39 +0100

    fct getAbsolutePath + test: Ok

--- src/arborescence.js
+++ src/arborescence.js
@@ -1,19 +1,27 @@
-function te() {
 
-}
 
-function Arborescence(tab) {
-    const map1 = new Map();
+function createTree(tab) { // format des elts du tableau: id, nom, parent
+    let map = new Map();
 
     tab.forEach(function (elt){
-        map1.set(elt[0], elt);
+        map.set(elt[0], elt);
     });
-
-    return map1;
+    return map;
 }
 
-function getAbsolutePath(tree, id) {
+function getAbsolutePath(table_tree, id) {
+    let tree_map = new Map();
+    tree_map = createTree(table_tree)
     
+    let t_ids = []; // table des ids
+    let t_racine = tree_map.get(id); // noeud considéré comme racine
+    t_ids.push(t_racine[0])
+
+    while (t_racine[2]!= -1){ //tant que le parent n'est pas egal a -1, on ajoute l id dans le tableau et on passe au noeud suivant
+        t_racine = tree_map.get(t_racine[2])
+        t_ids.push(t_racine[0])
+    }
+    return t_ids;
 }
 
-module.exports = Arborescence;
+module.exports =  getAbsolutePath;
--- test/Arborescence.test.js
+++ test/Arborescence.test.js
@@ -1,6 +1,26 @@
-const Arborescence = require("../src/arborescence.js");
+const createTree = require("../src/arborescence.js");
+const getAbsolutePath = require("../src/arborescence.js");
+/*
+let map = new Map();
+map.set(1, [1, "nom", "parent"]);
+map.set(2, [2, "nom2", "prenom"]);
+test('le min de t2 est plus grand que le max de t1 ',
+    () => {
+    let m = createTree([[1, "nom", "parent"], [2, "nom2", "prenom"]]);
+    let t = m.get(1);
+        expect(createTree([[1, "nom", "parent"], [2, "nom2", "prenom"]])).toStrictEqual(map);
+    });
+*/
+let map = new Map();
+map.set(0, [0,"r", -1]);
+map.set(1, [1,"fg", 0]);
+map.set(2, [2,"fd", 0]);
+map.set(3, [3,"fg", 2]);
+map.set(4, [4,"fg", 3]);
 
 
-test('le min de t2 est plus grand que le max de t1 ', () => {
-    expect().toBe();
+let mymap = [ [0,"r", -1], [1,"fg", 0], [2,"fd", 0], [3,"fg", 2],[4,"fg", 3] ] ;
+
+test('test getAbsolutePath', () => {
+    expect(getAbsolutePath(mymap,4)).toStrictEqual([2,0]);
   });
