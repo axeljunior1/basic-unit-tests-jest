95ae0a24b241e699303b5ad9ede09288fd62a094
axeljunior1 <68278948+axeljunior1@users.noreply.github.com>
2022-01-30 00:04:15 +0100

    Update Arborescence.test.js

--- test/Arborescence.test.js
+++ test/Arborescence.test.js
@@ -11,16 +11,23 @@ test('le min de t2 est plus grand que le max de t1 ',
         expect(createTree([[1, "nom", "parent"], [2, "nom2", "prenom"]])).toStrictEqual(map);
     });
 */
-let map = new Map();
-map.set(0, [0,"r", -1]);
-map.set(1, [1,"fg", 0]);
-map.set(2, [2,"fd", 0]);
-map.set(3, [3,"fg", 2]);
-map.set(4, [4,"fg", 3]);
 
 
-let mymap = [ [0,"r", -1], [1,"fg", 0], [2,"fd", 0], [3,"fg", 2],[4,"fg", 3] ] ;
 
-test('test getAbsolutePath', () => {
-    expect(getAbsolutePath(mymap,4)).toStrictEqual([2,0]);
+let tab = [ [0,"r", -1], [1,"fg", 0], [2,"fd", 0], [3,"fg", 2],[4,"fg", 3] ] ;
+
+test('test arborescence de la racine ', () => {
+    expect(getAbsolutePath(tab,0)).toStrictEqual([0]);
+  });
+
+test('test arborescence fils gauche ', () => {
+    expect(getAbsolutePath(tab,1)).toStrictEqual([1,0]);
+  });
+
+test('test arborescence fils droit ', () => {
+    expect(getAbsolutePath(tab,2)).toStrictEqual([2,0]);
+  });
+
+test('test arborescence un petit fils de la racine  ', () => {
+    expect(getAbsolutePath(tab,3)).toStrictEqual([3,2,0]);
   });
